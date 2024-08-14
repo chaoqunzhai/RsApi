072b586ec17e23d3caf42b4cb1fe9f5c989ac3fa
翟超群 <1047738527@qq.com>
2024-08-14 17:51:10 +0800

    fix ....

--- app/cmdb/models/rs_contract.go
+++ app/cmdb/models/rs_contract.go
@@ -12,9 +12,9 @@ type RsContract struct {
 	Desc           string                 `json:"desc" gorm:"type:varchar(35);comment:描述信息"`
 	Name           string                 `json:"name" gorm:"type:varchar(20);comment:合同名称"`
 	Number         string                 `json:"number" gorm:"type:varchar(20);comment:合同编号"`
-	BuId           int                    `json:"-" gorm:"type:bigint;comment:商务人员"`
-	CustomId       int                    `json:"-" gorm:"type:bigint;comment:所属客户ID"`
-	SignatoryId    int                    `json:"-" gorm:"type:bigint;comment:签订人"`
+	BuId           int                    `json:"buId" gorm:"type:bigint;comment:商务人员"`
+	CustomId       int                    `json:"customId" gorm:"type:bigint;comment:所属客户ID"`
+	SignatoryId    int                    `json:"signatoryId" gorm:"type:bigint;comment:签订人"`
 	User           string                 `json:"user" gorm:"type:varchar(20);comment:联系人名称"`
 	Type           int                    `json:"type" gorm:"type:bigint;comment:合同类型,contract_type"`
 	SettlementType int                    `json:"settlementType" gorm:"type:bigint;comment:结算方式,settlement_type"`
