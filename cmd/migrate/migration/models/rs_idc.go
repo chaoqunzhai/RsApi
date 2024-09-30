a42c7c5ad69c87029922e2c1ee38f76dfaaf6aff
翟超群 <1047738527@qq.com>
2024-09-30 12:31:07 +0800

    fix default

--- cmd/migrate/migration/models/rs_idc.go
+++ cmd/migrate/migration/models/rs_idc.go
@@ -10,8 +10,8 @@ type Idc struct {
 	CustomId      int    `json:"customId" gorm:"type:bigint;comment:所属客户ID"`
 	Region        string `json:"region" gorm:"type:varchar(120);comment:所在地区"`
 	Address       string `json:"address" gorm:"type:varchar(255);default:'';comment:详细地址"`
-	IpV6          bool   `json:"IpV6" gorm:"default:false;comment:是否IPV6"`
-	TypeId        int    `json:"type_id" gorm:"type:int(1);default:0;comment:机房类型"`
+	IpV6          bool   `json:"IpV6" gorm:"default:true;comment:是否IPV6"`
+	TypeId        int    `json:"type_id" gorm:"type:int(1);default:3;comment:机房类型"`
 	WechatName    string `json:"wechatName" gorm:"type:varchar(120);comment:企业微信群名称"`
 	WebHookUrl    string `json:"webHookUrl" gorm:"type:varchar(200);comment:企业微信webhookUrl"`
 	Status        int    `json:"status" gorm:"type:int(1);default:1;comment:机房状态"`
